99767e2bda62cb623e453dc0fb48a4b848306d37
Brayan chiquito <54116336+brayan-chiquito@users.noreply.github.com>
2023-01-27 16:48:12 -0500

    Add files via upload

---
+++ send more/send+moreV2.pl
@@ -0,0 +1,22 @@
+digito(0).
+digito(1).
+digito(2).
+digito(3).
+digito(4).
+digito(5).
+digito(6).
+digito(7).
+digito(8).
+digito(9).
+
+diferente([]).
+diferente([H|T]):-
+        not(member(H,T)),
+        diferente(T).
+
+solucion(L):-
+        digito(S),S>1,digito(E),digito(N),digito(D),
+        digito(M),M==1,digito(O),digito(R),digito(Y),
+        1000*S+100*E+10*N+D+1000*M+100*O+10*R+E =:= 10000*M+1000*O+100*N+10*E+Y,
+        L=[S,E,N,D,M,O,R,Y],
+        diferente(L).
